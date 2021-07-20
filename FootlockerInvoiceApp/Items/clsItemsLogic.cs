755564844d41a0a85b0f604b1ab264313110c874
William Allen <williamallen3@mail.weber.edu>
2021-07-20 14:55:56 -0600

    Added basic GUI, and started to make a Item class

--- FootlockerInvoiceApp/Items/clsItemsLogic.cs
+++ FootlockerInvoiceApp/Items/clsItemsLogic.cs
@@ -8,5 +8,22 @@ namespace FootlockerInvoiceApp.Items
 {
     class clsItemsLogic
     {
+        /// <summary>
+        /// the item code
+        /// </summary>
+        public int ItemCode { get; set; }
+        /// <summary>
+        /// name of the item
+        /// </summary>
+        public string ItemName { get; set; }
+        /// <summary>
+        /// cost of the item
+        /// </summary>
+        public double ItemCost { get; set; }
+        /// <summary>
+        /// short description of the item
+        /// </summary>
+        public string ItemDescription { get; set; }
+
     }
 }
