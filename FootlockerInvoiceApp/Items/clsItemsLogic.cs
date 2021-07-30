2e52947fd13582ca46f72d45ff3350f9c68eb9b6
William Allen <williamallen3@mail.weber.edu>
2021-07-29 20:58:14 -0600

    Added a couple comments to my code.

--- FootlockerInvoiceApp/Items/clsItemsLogic.cs
+++ FootlockerInvoiceApp/Items/clsItemsLogic.cs
@@ -6,24 +6,59 @@ using System.Threading.Tasks;
 
 namespace FootlockerInvoiceApp.Items
 {
-    class clsItemsLogic
+   public class clsItemsLogic
     {
-        /// <summary>
-        /// the item code
-        /// </summary>
-        public int ItemCode { get; set; }
-        /// <summary>
-        /// name of the item
-        /// </summary>
-        public string ItemName { get; set; }
-        /// <summary>
-        /// cost of the item
-        /// </summary>
-        public double ItemCost { get; set; }
-        /// <summary>
-        /// short description of the item
-        /// </summary>
-        public string ItemDescription { get; set; }
 
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public clsItemsLogic() 
+        {
+
+        }
+
+        //Need to make a binding list so invoices and other UI is updated when items are changed.
+        /// <summary>
+        /// This method will be used to pass all items in the database as a list of objects. 
+        /// </summary>
+        /// <returns></returns>
+        public List<Items> GetItems() 
+        {
+            List<Items> myList = new List<Items>();
+            return myList;
+        }
+
+        /// <summary>
+        /// class to hold objects of data passed in from the data base.
+        /// </summary>
+        public class Items
+        {
+            /// <summary>
+            /// the item code
+            /// </summary>
+            public int ItemCode { get; set; }
+            /// <summary>
+            /// name of the item
+            /// </summary>
+            public string ItemName { get; set; }
+            /// <summary>
+            /// cost of the item
+            /// </summary>
+            public double ItemCost { get; set; }
+            /// <summary>
+            /// short description of the item
+            /// </summary>
+            public string ItemDescription { get; set; }
+        }
+
+        //need a function to get a list of all invoices to see if an item being delted is on an exsisting invoice.
+
+        //if the item is on an invoice tell the user the invoice id with the item.
+
+        //add items
+
+        //edit items (do not let the code be updated.)
+
+        //delete items
     }
 }
